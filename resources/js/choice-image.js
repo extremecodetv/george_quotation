6d77e2cf1e4f458c3c6bb8c714ee39954c66141e
Artem Dontsov <extremecodetv@gmail.com>
2024-09-28 21:03:16 +0700

    Some shit

--- resources/js/choice-image.js
+++ resources/js/choice-image.js
@@ -1,25 +1,13 @@
 $(async function () {
 
-    async function fetchBlob(url) {
-        const response = await fetch(url);
-    
-        // Here is the significant part 
-        // reading the stream as a blob instead of json
-        return response.blob();
-    }
-
-    const reader = new FileReader()
-    reader.onload = function () {
-        var meme = new Image()
-        meme.src = 'https://github.com/extremecodetv/george_quotation/blob/master/template.png?raw=true'
-        meme.onload = function () {
-            var imgInfo = {
-                url: reader.result,
-                height: meme.height,
-                width: meme.width,
-            }
-            $('.choice-section').trigger('choice-done', imgInfo)
+    var meme = new Image()
+    meme.src = 'https://github.com/extremecodetv/george_quotation/blob/master/template.png?raw=true'
+    meme.onload = function () {
+        var imgInfo = {
+            url: reader.result,
+            height: meme.height,
+            width: meme.width,
         }
+        $('.choice-section').trigger('choice-done', imgInfo)
     }
-    reader.readAsDataURL((await fetchBlob('blob:https://github.com/extremecodetv/george_quotation/blob/master/template.png?raw=true')))
 })
