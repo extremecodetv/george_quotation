ada28688c4274b3ee1715778b4d770a04be33552
Artem Dontsov <extremecodetv@gmail.com>
2024-09-28 21:07:05 +0700

    Some shit

--- resources/js/choice-image.js
+++ resources/js/choice-image.js
@@ -1,10 +1,18 @@
 $(async function () {
 
+    async function fetchBlob(url) {
+        const response = await fetch(url);
+    
+        // Here is the significant part 
+        // reading the stream as a blob instead of json
+        return response.arrayBuffer();
+    }
+
     var meme = new Image()
     meme.src = 'https://github.com/extremecodetv/george_quotation/blob/master/template.png?raw=true'
     meme.onload = function () {
         var imgInfo = {
-            url: reader.result,
+            url:'https://github.com/extremecodetv/george_quotation/blob/master/template.png?raw=true',
             height: meme.height,
             width: meme.width,
         }
